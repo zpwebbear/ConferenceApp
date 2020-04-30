c1b1445bc4255cd062eb265784edddaf816a8007
zpwebbear <zp.babu@gmail.com>
2020-04-30 12:05:04 +0300

    Add app seeder

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ConferenceApp.Data;
+using ConferenceApp.Models;
 
 namespace ConferenceApp
 {
@@ -15,7 +18,31 @@ namespace ConferenceApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+         private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<ConferenceAppContext>();
+                    context.Database.EnsureCreated();
+                    ConferenceAppInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
