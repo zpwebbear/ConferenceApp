be61b571203482fa635ee05890ac739a6ec9a7b0
zpwebbear <zp.babu@gmail.com>
2020-04-30 13:01:33 +0300

    Add participant timestamps

--- Models/Participant.cs
+++ Models/Participant.cs
@@ -9,21 +9,26 @@ namespace ConferenceApp.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        
-        [Column(TypeName="Date")]
+
+        [Column(TypeName = "Date")]
         public DateTime DateOfArrival { get; set; }
 
-        [Column(TypeName="Date")]
+        [Column(TypeName = "Date")]
         public DateTime DateOfDeparture { get; set; }
         public string CompanyName { get; set; }
         public string PositionInCompany { get; set; }
         public ParticipantRole Role { get; set; }
-        public Gender Gender {get; set;}
+        public Gender Gender { get; set; }
 
-        [Column(TypeName="Date")]
+        [Column(TypeName = "Date")]
         public DateTime BirthDate { get; set; }
-        public Country Country { get; set;}
+        public Country Country { get; set; }
 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Inserted { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastUpdated { get; set; }
     }
 
     public class ParticipantRole
